5f484ae2eec7466b747a741b4c73a35f253aa617
Jianzhi Wang <jianzhiw00@gmail.com>
2025-06-19 13:10:20 +0800

    Update command.sh

--- command.sh
+++ command.sh
@@ -3,3 +3,7 @@ nvidia-smi # system management interface
 # nvcc = nvidia cuda compiler
 nvcc -o out hello.cu -run # compiles hello.cu into out
 # run executes the compiled out (convenience)
+
+# nsys = nsight systems
+!nsys profile --stats=true ./out
+# essentially a cuda program profiler
