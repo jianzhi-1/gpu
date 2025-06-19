63e70f6b54e797a2d1b948938321663ff50150b4
Jianzhi Wang <jianzhiw00@gmail.com>
2025-06-19 11:26:35 +0800

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 nvidia-smi # system management interface
 # nvcc = nvidia cuda compiler
-nvcc -o hello hello.cu -run # compiles hello.cu into hello
+nvcc -o out hello.cu -run # compiles hello.cu into out
