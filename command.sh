18bab3af709cbd69f415048b39ca3aba182f8f03
Jianzhi Wang <jianzhiw00@gmail.com>
2025-06-19 14:50:01 +0800

    Update command.sh

--- command.sh
+++ command.sh
@@ -5,5 +5,6 @@ nvcc -o out hello.cu -run # compiles hello.cu into out
 # run executes the compiled out (convenience)
 
 # nsys = nsight systems
+!nsys --version
 !nsys profile --stats=true ./out
 # essentially a cuda program profiler
